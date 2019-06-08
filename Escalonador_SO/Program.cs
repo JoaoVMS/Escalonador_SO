e75af081e5afe35ef43162f0c9cfe76ff2ff8f0e
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2019-06-07 21:49:08 -0300

    Mudei o menu principal

--- Escalonador_SO/Program.cs
+++ Escalonador_SO/Program.cs
@@ -16,7 +16,7 @@ namespace Escalonador_SO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
