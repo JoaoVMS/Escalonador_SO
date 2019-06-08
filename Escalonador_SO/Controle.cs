67554607ff7c78d29ac6e1044e74c9fab7fa2fab
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2019-06-08 14:14:06 -0300

    Alterações na classe controle.

--- Escalonador_SO/Controle.cs
+++ Escalonador_SO/Controle.cs
@@ -14,47 +14,5 @@ namespace Escalonador_SO
 
         readonly static string nomeArquivo = "processos.txt";
 
-        /// <summary>
-        /// Aloca o processo na fila de processos correta
-        /// </summary>
-        public static void LeituraArquivo()
-        {
-            if (!File.Exists(nomeArquivo))
-            {
-                //Exibir mensagem de erro "Arquivo não foi encontrado ou não existe."
-
-                return;
-            }
-
-            string[] info;
-
-            //Fazer a leitura do arquivo e organizar entre as 10 filas
-            using (StreamReader entrada = new StreamReader(nomeArquivo))
-            {
-                while (!entrada.EndOfStream)
-                {
-                    info = entrada.ReadLine().Split(';');
-                    try
-                    {
-                        if (info.Length == 4)
-                        {
-                            Processo processo = new Processo(Convert.ToInt32(info[0]), info[1], Convert.ToInt32(info[2]), Convert.ToInt32(info[3]));
-                            Escalonador.AdicionarProcesso(processo);
-                            this.AdicionarListView(processo); //Esse this é referente a quê? 
-                        }
-                        else
-                        {
-                            //Exibir mensagem de erro "falta atributos"
-
-                        }
-                    }
-                    catch (Exception erro)
-                    {
-                        //Exibir mensagem de erro de conversão
-
-                    }
-                }
-            }
-        }
     }
 }
