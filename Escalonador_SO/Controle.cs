80714c7a9b54afcfd087d2e5b226b1a096650a53
Gustavo Sena <gustavosena98@LIVE.COM>
2019-06-08 17:49:57 -0300

    Remover e inserir itens das listviews

--- Escalonador_SO/Controle.cs
+++ Escalonador_SO/Controle.cs
@@ -5,17 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Todas_as_Estruturas_de_Dados;
-
+using System.Windows.Forms;
 
 namespace Escalonador_SO
 {
     class Controle
     {
+        //Escalonador
         public static int sleep = 300, trocaContexto = 200, quantum = 500;
         public static Escalonador Escalonador = new Escalonador(quantum, trocaContexto, sleep);
-        //public static Processo proc_aux;
         public static Fila FilaProcessosSuspensos = new Fila();
+
+        //Arquivo onde tem todos os processos
         public readonly static string nomeArquivo = "processos.txt";
 
+        //ListView
+        public static ListView ListViewSuspensa;
+        public static ListView[] ListViewsFilasProcesso = new ListView[10];
+
+        /// <summary>
+        /// Adiciona o processo ao listView Correto
+        /// </summary>
+        /// <param name="processo">Processo que será adicionado ao listView</param>
+        public static void AdicionarListView(Processo processo)
+        {
+            ListViewItem aux = new ListViewItem(Convert.ToString(processo.PID));
+            aux.SubItems.Add(processo.Nome);
+            aux.SubItems.Add(Convert.ToString(processo.Prioridade));
+            aux.SubItems.Add(Convert.ToString(processo.QtdeCiclos));
+
+            ListViewsFilasProcesso[processo.Prioridade - 1].Items.Add(aux);
+        }
+
+        /// <summary>
+        /// Retira o processo na fila de processos
+        /// </summary>
+        /// <param name="processo">Processo que será retirado</param>
+        public static void RetirarListView(Processo processo)
+        {
+            ListViewsFilasProcesso[processo.Prioridade - 1].Items.Remove(ListViewsFilasProcesso[processo.Prioridade - 1].FindItemWithText(Convert.ToString(processo.PID)));
+        }
+
+        /// <summary>
+        /// Insere o processo na listView com processos que foram suspensos
+        /// </summary>
+        /// <param name="processo">Processo que será enviado para a listView Suspesos</param>
+        public static void InserirListViewSuspensa(Processo processo)
+        {
+            ListViewItem aux = new ListViewItem(Convert.ToString(processo.PID));
+            aux.SubItems.Add(processo.Nome);
+            aux.SubItems.Add(Convert.ToString(processo.Prioridade));
+            aux.SubItems.Add(Convert.ToString(processo.QtdeCiclos));
+
+            ListViewSuspensa.Items.Add(aux);
+        }
     }
 }
+    
--- Escalonador_SO/Form1.cs
+++ Escalonador_SO/Form1.cs
@@ -15,88 +15,17 @@ namespace Escalonador_SO
 {
     public partial class Form1 : Form
     {
-        Escalonador Escalonador { get; set; }
-
         public Form1()
         {
             InitializeComponent();
-            LeituraArquivo();
-            //Da pra transformar aquele tanto de listViewers em um vetor, por causa da referência dos objetos ListView
-            ListView[] listViews = { listViewFilaPrioridade1 , listViewFilaPrioridade2, listViewFilaPrioridade3, listViewFilaPrioridade4, listViewFilaPrioridade4, listViewFilaPrioridade6, listViewFilaPrioridade7, listViewFilaPrioridade8, listViewFilaPrioridade9, listViewFilaPrioridade10};
-        }
-
-        #region Métodos Auxiliares
-        /// <summary>
-        /// Adiciona o processo ao listView Correto
-        /// </summary>
-        /// <param name="processo">Processo que será adicionado ao listView</param>
-        private void AdicionarListView(Processo processo)
-        {
-            
-            ListViewItem aux = new ListViewItem(Convert.ToString(processo.PID));
-            aux.SubItems.Add(processo.Nome);
-            aux.SubItems.Add(Convert.ToString(processo.Prioridade));
-            aux.SubItems.Add(Convert.ToString(processo.QtdeCiclos));
-
-            switch (processo.Prioridade)
-            {
-                case 1: listViewFilaPrioridade1.Items.Add(aux); break;
-                case 2: listViewFilaPrioridade2.Items.Add(aux); break;
-                case 3: listViewFilaPrioridade3.Items.Add(aux); break;
-                case 4: listViewFilaPrioridade4.Items.Add(aux); break;
-                case 5: listViewFilaPrioridade5.Items.Add(aux); break;
-                case 6: listViewFilaPrioridade6.Items.Add(aux); break;
-                case 7: listViewFilaPrioridade7.Items.Add(aux); break;
-                case 8: listViewFilaPrioridade8.Items.Add(aux); break;
-                case 9: listViewFilaPrioridade9.Items.Add(aux); break;
-                case 10: listViewFilaPrioridade10.Items.Add(aux); break;
-                default: break;
-            }
-        }
-
-
-        /// <summary>
-        /// Retira o processo na fila de processos
-        /// </summary>
-        /// <param name="processo">Processo que será retirado</param>
-        //public void RetirarListView(Processo processo)
-        //{
-        //    switch (processo.Prioridade)
-        //    {
-        //        case 1: listViewFilaPrioridade1.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 2: listViewFilaPrioridade2.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 3: listViewFilaPrioridade3.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 4: listViewFilaPrioridade4.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 5: listViewFilaPrioridade5.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 6: listViewFilaPrioridade6.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 7: listViewFilaPrioridade7.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 8: listViewFilaPrioridade8.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 9: listViewFilaPrioridade9.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        case 10: listViewFilaPrioridade10.Items.RemoveByKey(Convert.ToString(processo.PID)); break;
-        //        default: Console.WriteLine("Erro: "); break;
-        //    }
-        //}
-        #endregion
-
-        #region Botões
-        private void SairToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            DialogResult result = MessageBox.Show("O processo será perdido, tem certeza que deseja fechar o escalonador?", "Sair", MessageBoxButtons.YesNo);
-            if (result == System.Windows.Forms.DialogResult.Yes)
-                this.Close();
-        }
-        private void LeituraDoArquivoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //ler arquivo e colocar no escalonador
-            //Colocar na listview também
+            InicializarListViews();
             LeituraArquivo();
         }
-        #endregion
 
         /// <summary>	
         /// Aloca o processo na fila de processos correta	
         /// </summary>	
-        public void LeituraArquivo()	
+        public void LeituraArquivo()
         {	
             if (!File.Exists(Controle.nomeArquivo))	
             {	
@@ -119,7 +48,7 @@ namespace Escalonador_SO
                         {	
                             Processo processo = new Processo(Convert.ToInt32(info[0]), info[1], Convert.ToInt32(info[2]), Convert.ToInt32(info[3]));	
                             Controle.Escalonador.AdicionarProcesso(processo);	
-                            AdicionarListView(processo); //Esse this é referente a quê? 	
+                            Controle.AdicionarListView(processo); //Esse this é referente a quê? 	
                         }	
                         else	
                         {	
@@ -135,6 +64,39 @@ namespace Escalonador_SO
                 }	
             }	
         }	
+        
+        /// <summary>	
+        /// A classe Controle recebe o ponteiro que aponta pras listViews
+        /// </summary>	
+        public void InicializarListViews()
+        {
+            //Da pra transformar aquele tanto de listViewers em um vetor, por causa da referência dos objetos ListView
+            Controle.ListViewsFilasProcesso[0] = listViewFilaPrioridade1;
+            Controle.ListViewsFilasProcesso[1] = listViewFilaPrioridade2;
+            Controle.ListViewsFilasProcesso[2] = listViewFilaPrioridade3;
+            Controle.ListViewsFilasProcesso[3] = listViewFilaPrioridade4;
+            Controle.ListViewsFilasProcesso[4] = listViewFilaPrioridade5;
+            Controle.ListViewsFilasProcesso[5] = listViewFilaPrioridade6;
+            Controle.ListViewsFilasProcesso[6] = listViewFilaPrioridade7;
+            Controle.ListViewsFilasProcesso[7] = listViewFilaPrioridade8;
+            Controle.ListViewsFilasProcesso[8] = listViewFilaPrioridade9;
+            Controle.ListViewsFilasProcesso[9] = listViewFilaPrioridade10;
+            Controle.ListViewSuspensa = listViewFilaSuspesa;
+        }
+
+        #region Botões
+        private void SairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("O processo será perdido, tem certeza que deseja fechar o escalonador?", "Sair", MessageBoxButtons.YesNo);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+                this.Close();
+        }
+        private void LeituraDoArquivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ler arquivo e colocar no escalonador
+            //Colocar na listview também
+            LeituraArquivo();
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -146,5 +108,6 @@ namespace Escalonador_SO
             //Criar novo forms para pegar os dados que a pessoa quiser para criar o novo processo
             //Pego esse processo, crio, e por meio da prioridade dele encaixo em sua respectiva fila de processos
         }
+        #endregion
     }
 }
